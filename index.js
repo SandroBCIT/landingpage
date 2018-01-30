f45bc5a6962757855d15873de59f940f766cb171
unknown <alessandro.grunwald@gmail.com>
2018-01-30 14:57:14 -0800

    a

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, resp)=>{
-    resp.end("Hi, Welcime to my heroku app");
+    resp.end("Hi, Welcome to my heroku app");
 });
 
 app.listen(port, (err)=>{
